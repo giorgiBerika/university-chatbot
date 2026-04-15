66ab4748f6a6767710bf1f844d0e7956831dfd59
giorgiBerika <giorgi.berikashvili.3@iliauni.edu.ge>
2026-04-15 16:29:21 +0400

    Initial project setup: config, data folder, requrements

---
+++ config.py
@@ -0,0 +1,11 @@
+
+# General configuration 
+
+LLM = "llama3.1:8b"
+OLLAMA_SERVER = "http://localhost:11434"
+EMBEDDINGS = "BAAI/bge-small-en-v1.5"
+CHROMADB_PATH = "data/chroma_db"
+COLLECTION_NAME = "documents"
+CHUNK_SIZE = 256
+CHUNK_OVERLAP = int( ( CHUNK_SIZE * 10 ) / 100 )
+RETRIEVE = 3
