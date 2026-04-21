4f88f86588f0d1a7305e08a96fe1c6e317031a11
giorgiBerika <giorgi.berikashvili.3@iliauni.edu.ge>
2026-04-21 15:43:25 +0400

    A document loader and chunker with recursive text splitting.

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ LLM = "llama3.1:8b"
 OLLAMA_SERVER = "http://localhost:11434"
 EMBEDDINGS = "BAAI/bge-small-en-v1.5"
 CHROMADB_PATH = "data/chroma_db"
-DATA_PATH = "../data/raw"
+DATA_PATH = "data/raw"
 
 COLLECTION_NAME = "documents"
 CHUNK_SIZE = 256
