c733d98d36048a5a7b5e4931ae61bdcf55942dc4
giorgiBerika <giorgi.berikashvili.3@iliauni.edu.ge>
2026-04-28 17:47:40 +0400

    Moving the calling sentence-transformer line out of the function

--- retrieval/search.py
+++ retrieval/search.py
@@ -2,9 +2,9 @@ from sentence_transformers import SentenceTransformer
 import chromadb
 from config import CHROMADB_PATH, EMBEDDINGS, RETRIEVE
 
-
+model = SentenceTransformer(EMBEDDINGS)
 def search_in_db( user_question ):
-    model = SentenceTransformer(EMBEDDINGS)
+   
     chroma_client = chromadb.PersistentClient(path=CHROMADB_PATH)
     collection = chroma_client.get_or_create_collection(name="iliauni_info")
 
